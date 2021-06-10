3c4302edd0b8e6e83c578e5d7ef48d628d8a24ff
David <djaime93@gmail.com>
2021-06-09 23:15:26 -0500

    Add styling to pokeman card

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,6 +8,9 @@
 </svelte:head>
 <h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
 
-{#each $pokemon as pokeman}
+<div class="grid gap-4 md:grid-cols-2 grid cols-1">
+
+    {#each $pokemon as pokeman}
     <PokemanCard pokeman={pokeman}/>
-{/each}
+    {/each}
+</div>
